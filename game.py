798fe8b4317918c3783dbf6d1c61d7bfbee77cc6
kyn <muenpan_k@silpakorn.edu>
2025-07-29 09:52:19 +0700

    Update game.py

--- game.py
+++ game.py
@@ -12,6 +12,23 @@ def get_player_guess():
     """
     # Student 1: Add your code here
     pass
+    while True:
+
+        try:
+
+            guess = int(input("Enter your guess (1-100): "))
+
+            if 1 <= guess <= 100:
+
+                return guess
+
+            else:
+
+                print("Please enter a number between 1 and 100.")
+
+        except ValueError:
+
+            print("Invalid input. Please enter a valid integer.")
 
 def check_guess(secret_number, player_guess):
     """
